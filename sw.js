6f184c36a22c5fe0c258486cca3446e51c5a97e9
SimplePrimate <ecleek@gmail.com>
2020-07-16 14:45:44 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f7d023a85ce27dd67be0.js"
+    "url": "webpack-runtime-19c09df3ce0cda503bc7.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "58cf696fa2309b30503d94c2f3c141cc"
+    "revision": "f13f04a9fc8bf43600989e70194ee1cd"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
